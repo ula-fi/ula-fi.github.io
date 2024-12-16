21fd3b0dcd44855da3687643722bc577f8cec520
ula-fi <ulaf51233@gmail.com>
2024-12-17 00:58:52 +0700

    sayt

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -97,7 +97,8 @@ function jsCopy() {
 function copy() {
   return gulp
     .src(resources.static, {
-      base: "src"
+      base: "src",
+      encoding: false
     })
     .pipe(gulp.dest("dist/"));
 }
